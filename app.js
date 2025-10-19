41384f683036a1e7903a429f2473099f5a71f0ea
orokidd <gregomily@gmail.com>
2025-10-19 14:46:49 +0800

    new message view and route

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
 const indexRouter = require('./routes/indexRouter');
+const newMessageRouter = require('./routes/newMessageRouter');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use('/', indexRouter);
-app.use(express.urlencoded({ extended: true }));
+app.use('/new', newMessageRouter);
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
