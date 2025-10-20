de891ec516c48f53d20c65253aa7d9ed23df1f21
orokidd <gregomily@gmail.com>
2025-10-20 13:19:54 +0800

    added classes and css file

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/new', newMessageRouter);
