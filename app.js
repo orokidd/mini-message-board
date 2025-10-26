e99d1a8dfec19b2b51f9bf046653c97e7c6c5f0f
orokidd <gregomily@gmail.com>
2025-10-26 19:17:20 +0800

    change port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/new', newMessageRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
   if (error) {
     throw error;
