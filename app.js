63810d840469c7a1c2e9db193eaf7ebc644789ea
orokidd <gregomily@gmail.com>
2025-10-29 17:37:44 +0800

    add host in app js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/', indexRouter);
 app.use('/new', newMessageRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, (error) => {
+app.listen(PORT, "0.0.0.0", (error) => {
   if (error) {
     throw error;
   }
